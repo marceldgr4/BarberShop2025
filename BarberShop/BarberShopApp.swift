27bfac3becbb98a7f9b51504948be0a92b88ce5e
marceldgr4 <marceldgr@gmail.com>
2025-11-18 23:13:04 -0500

    Initial Commit

---
+++ BarberShop/BarberShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarberShopApp.swift
+//  BarberShop
+//
+//  Created by Marcel DiazGranados Robayo on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BarberShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarberShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarberShop
+//
+//  Created by Marcel DiazGranados Robayo on 18/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
