2d18bd907410a8fa01e605f9cf52344bf1aec0ce
marceldgr4 <marceldgr@gmail.com>
2026-01-15 21:30:41 -0500

    update

---
+++ BarberShop/View/AvailabitliyTab/AvailabilityTabView.swift
@@ -0,0 +1,18 @@
+//
+//  AvailabilityTabView.swift
+//  BarberShop
+//
+//  Created by Marcel DiazGranados Robayo on 15/01/26.
+//
+
+import SwiftUI
+
+struct AvailabilityTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AvailabilityTabView()
+}
---
+++ BarberShop/View/AvailabitliyTab/Card/DayAvailabilityRow.swift
@@ -0,0 +1,18 @@
+//
+//  DayAvailabilityRow.swift
+//  BarberShop
+//
+//  Created by Marcel DiazGranados Robayo on 15/01/26.
+//
+
+import SwiftUI
+
+struct DayAvailabilityRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DayAvailabilityRow()
+}
